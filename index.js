244e1ac000a612767ad35d3f9b92a5c0842ee7b1
Harshit <geraharshitkumar@gmail.com>
2021-02-11 04:19:55 +0530

    use 'npm start' for starting server instead of nodemon index.js'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-// use express router for requiring all routes.
+// use express router for requiring all routes
 app.use('/', require('./routes/index'));
 
 
