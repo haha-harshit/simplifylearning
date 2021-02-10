53b7a575b795021841da7eed29294a0ee3375e87
Harshit <geraharshitkumar@gmail.com>
2021-02-11 04:34:46 +0530

    added controller

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ const express = require('express');
 // router
 const router = express.Router();
 
-console.log('router loaded!')
+
+const homeController = require('../controllers/home_controller');
+
+router.get('/', homeController.home);
+
+
+console.log('router loaded!');
 
 // exporting router to use in index.js
 module.exports = router
